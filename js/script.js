25dabd07e9de4a61158cd174a492f2ebe96fb513
Laassel <lassel.giovanni@gmail.com>
2024-10-15 15:45:00 +0200

    End of the cycle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //Ex-FizzBuzz
 
-//Inizio un ciclo che va da 1 a 100
+//Inizio del ciclo For che va da 1 a 100
 for (let i = 0; i < 100; i++) {
     let numb = i + 1;
     console.log(numb);
 
-//Verifico se i numeri siano divisibili sia per 3 che per 5 
+//Verifico se i numeri siano divisibili sia per 3 che per 5. Creo le variabili 
     // SE vero, stampo "FizzBuzz"
     let number3 = 3;
     let number5 = 5;
@@ -25,5 +25,5 @@ for (let i = 0; i < 100; i++) {
         if(numb % number5 === 0) {
         console.log(`${numb}: Buzz`)
     }
-
+// Fine Ciclo, se il numero non è divisibile ne per 3 ne per 5 verrà stampato il numero stesso.
 };
