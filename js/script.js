a25627ce5cceb1b85d3550f07940256e4d7b29ab
Laassel <lassel.giovanni@gmail.com>
2024-10-15 15:27:21 +0200

    Add FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 //Ex-FizzBuzz
+
 //Inizio un ciclo che va da 1 a 100
 for (let i = 0; i < 100; i++) {
+    let numb = i + 1;
+    console.log(numb);
 
-}
+//Verifico se i numeri siano divisibili sia per 3 che per 5 
+    // SE vero, stampo "FizzBuzz"
+    let number3 = 3;
+    let number5 = 5;
+
+    if(numb % number3 === 0 && numb % number5 ===0) {
+        console.log(`${numb}: FizzBuzz`);
+    };
+    
+};
